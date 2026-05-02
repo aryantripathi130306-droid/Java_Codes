5c99f6b14b0fcf8843296f41f756f5a35a479299
aryantripathi130306-droid <aryantripathi130306@gmail.com>
2026-05-02 13:37:11 +0530

    Digit count

---
+++ Digitcount.java
@@ -0,0 +1,12 @@
+public class Digitcount {
+    public static void main(String[] args){
+        int num=12345;
+        int count=0;
+        while(num != 0){
+            num = num / 10;
+            count++;
+        }
+        System.out.println("The number of digits in the given number is: "+count);
+    }
+    
+}
